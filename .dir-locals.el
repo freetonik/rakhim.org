66e35e3a1130b410e1d72b8d50f76d38e0055e82
Rakhim Davletkaliyev <contact@rakhim.org>
2020-07-06 09:58:19 +0300

    markdown dnd local path modifier

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((markdown-mode
+  (dnd-drop-part-img-path . "/Users/rakhim/code/rakhim.org/static")
   (dnd-save-directory . "../../static/images/posts/")))
